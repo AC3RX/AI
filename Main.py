2cdaef63cf6eb379c13409e1dbccf6c23c58294d
AC3RX <progame.neon@gmail.com>
2025-07-02 17:33:01 +0600

    Ai Chatbot

---
+++ Main.py
@@ -0,0 +1,16 @@
+# name 
+name = str(input("Hey whats good? what is ur name?"))
+
+print(f"nice to meet ya {name}")
+mood = str(input("how are you feeling?").lower())
+
+if mood == "good": 
+    print(("That is nice to hear."))
+
+elif mood == "bad":
+    print("Aw shucks, well dont worry its gonna be fine.")
+
+else : 
+    print("Oh well if you dont waant to talk about it, I understand.")
+
+print(f"Well that was a good talk, see you later {name}")
